bc7d2bdb410f724bd944253b845fdd0205760e45
ultrasev <i@ultrasev.com>
2024-11-18 16:04:31 +0800

    feat: add JSON encoding feature

--- examples/main.go
+++ examples/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/ultrasev/gofast/pkg/json"
 	"github.com/ultrasev/gofast/pkg/math"
 )
 
 func main() {
 	result := math.Min(10, 20)
 	fmt.Printf("Minimum value: %d\n", result)
+
+	response, err := json.Encode(map[string]string{"hello": "world"})
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+	fmt.Println("JSON response:", string(response))
 }
