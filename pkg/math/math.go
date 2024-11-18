583a6655540737f5ce1e188d4fa65b2d018c107b
ultrasev <i@ultrasev.com>
2024-11-18 15:50:37 +0800

    style: format with go

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -2,16 +2,16 @@ package math
 
 // Min returns the smaller of x or y.
 func Min[T ~int | ~float64](x, y T) T {
-    if x < y {
-        return x
-    }
-    return y
+	if x < y {
+		return x
+	}
+	return y
 }
 
 // Max returns the larger of x or y.
 func Max[T ~int | ~float64](x, y T) T {
 	if x > y {
-        return x
-    }
+		return x
+	}
 	return y
 }
--- pkg/math/math_test.go
+++ pkg/math/math_test.go
@@ -3,21 +3,21 @@ package math
 import "testing"
 
 func TestMin(t *testing.T) {
-    tests := []struct {
-        name     string
-        x, y     int
-        expected int
-    }{
-        {"positive numbers", 10, 20, 10},
-        {"negative numbers", -10, -20, -20},
-        {"equal numbers", 10, 10, 10},
-    }
+	tests := []struct {
+		name     string
+		x, y     int
+		expected int
+	}{
+		{"positive numbers", 10, 20, 10},
+		{"negative numbers", -10, -20, -20},
+		{"equal numbers", 10, 10, 10},
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            if got := Min(tt.x, tt.y); got != tt.expected {
-                t.Errorf("Min() = %v, want %v", got, tt.expected)
-            }
-        })
-    }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Min(tt.x, tt.y); got != tt.expected {
+				t.Errorf("Min() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
 }
